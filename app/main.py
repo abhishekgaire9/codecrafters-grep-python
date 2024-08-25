1d48879c7ccf6a5595b340b4e0a3aa29a6ae9566
Abhishek Gaire <abhishekgaire011@gmail.com>
2024-08-25 10:19:52 +0545

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,98 +1,44 @@
 import sys
-# import pyparsing - available if you need it!
+# import pyparsing
 # import lark - available if you need it!
-def matcher(input_line, pattern):
-    ptr1 = 0
-    ptr2 = 0
-    if input_line == "" and pattern == "":
+def match_local(input_line: str, pattern: str):
+    if len(pattern) == 0:
         return True
-    elif input_line == "":
-        return False
-    elif pattern == "":
-        return True
-    while ptr1 < len(input_line):
-        if ptr2 + 1 < len(pattern) and pattern[ptr2 : ptr2 + 2] == "\\d":
-            if input_line[ptr1].isdigit():
-                return matcher(input_line[ptr1 + 1 :], pattern[ptr2 + 2 :])
-            else:
-                ptr1 = ptr1 + 1
-        elif ptr2 + 1 < len(pattern) and pattern[ptr2 : ptr2 + 2] == "\\w":
-            if input_line[ptr1].isalnum():
-                return matcher(input_line[ptr1 + 1 :], pattern[ptr2 + 2 :])
-            else:
-                ptr1 = ptr1 + 1
-        elif input_line[ptr1] == pattern[ptr2]:
-            return matcher(input_line[ptr1 + 1 :], pattern[ptr2 + 1 :])
-        else:
-            ptr1 = ptr1 + 1
-    return False
-def match_pattern(input_line, pattern):
-    if len(pattern) == 1:
-        return pattern in input_line
-    elif pattern == "\\d":
-        return any(char.isdigit() for char in input_line)
-    elif pattern == "\\w":
-        return any(char.isalnum() for char in input_line)
-    elif pattern[0:2] == "[^":
-        pat = pattern[2:-1]
-        return all(char not in input_line for char in pat)
-    elif pattern[0] == "[" and pattern[-1] == "]":
-        pat = pattern[1:-1]
-        return any(char in input_line for char in pat)
+    elif pattern.startswith("\d"):
+        return input_line[0].isdigit() and match_local(input_line[1:], pattern[2:])
+    elif pattern.startswith("\w"):
+        return input_line[0].isalnum() and match_local(input_line[1:], pattern[2:])
+    elif pattern.startswith("[^") and "]" in pattern:
+        chars = pattern[1 : pattern.index("]")]
+        return input_line[0] not in chars and match_local(
+            input_line[1:], pattern[pattern.index("]") + 1 :]
+        )
+    elif pattern.startswith("[") and "]" in pattern:
+        chars = pattern[1 : pattern.index("]")]
+        return input_line[0] in chars and match_local(
+            input_line[1:], pattern[pattern.index("]") + 1 :]
+        )
+    elif pattern[0] == input_line[0]:
+        return match_local(input_line[1:], pattern[1:])
     else:
-        return matcher(input_line, pattern)
-
-def matcher(input_line, pattern):
-    if not input_line and not pattern:
-        return True
-    if not pattern:
-        return True
-    if not input_line:
+        raise RuntimeError(f"Unhandled pattern: {pattern}")
         return False
-
-    if len(pattern) >= 2 and pattern[:2] in ["\\d", "\\w"]:
-        if pattern[:2] == "\\d" and input_line[0].isdigit():
-            return matcher(input_line[1:], pattern[2:])
-        elif pattern[:2] == "\\w" and input_line[0].isalnum():
-            return matcher(input_line[1:], pattern[2:])
-        return matcher(input_line[1:], pattern)
-    elif input_line[0] == pattern[0]:
-        return matcher(input_line[1:], pattern[1:])
+def match_pattern(input_line: str, pattern: str):
+    if pattern[0] == "^":
+        return match_local(input_line, pattern[1:])
+    if match_local(input_line, pattern):
+        return True
     else:
-        return matcher(input_line[1:], pattern)
-# def match_pattern(input_line, pattern):
-#     if len(pattern) == 1:
-#         return pattern in input_line
-#     elif pattern == "\\d":
-#         for i in range(10):
-#             if input_line.find(str(i)) != -1:
-#                 return True
-#     elif pattern == "\\w":
-#         return input_line.isalnum()
-#     elif pattern[0:2] == "[^":
-#         pat = pattern[2:-1]
-#         for ch in pat:
-#             if input_line.find(ch) != -1:
-#                 return False
-#         return True
-#     elif pattern[0] == "[" and pattern[-1] == "]":
-#         pat = pattern[1:-1]
-#         for ch in pat:
-#             if input_line.find(ch) != -1:
-#                 return True
-#         return False
-#     else:
-#         raise RuntimeError(f"Unhandled pattern: {pattern}")
-#         return matcher(input_line, pattern)
+        truncated = input_line[1:]
+        if len(truncated) == 0 or (len(truncated) == 1 and truncated[0] == "\n"):
+            return False
+        return match_pattern(truncated, pattern)
 def main():
     pattern = sys.argv[2]
     input_line = sys.stdin.read()
     if sys.argv[1] != "-E":
         print("Expected first argument to be '-E'")
         exit(1)
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-    # Uncomment this block to pass the first stage
     if match_pattern(input_line, pattern):
         exit(0)
     else:
